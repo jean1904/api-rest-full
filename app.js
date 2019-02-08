666d7350e112714554c68ce7e56ef600f8ea3fa4
Jeliteck <creativo-02@virtualplaza.net>
2019-02-08 11:41:02 -0500

    CORS

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var noticias = require('./data/noticias.json');
 
 app.get('', function (req, res) {
